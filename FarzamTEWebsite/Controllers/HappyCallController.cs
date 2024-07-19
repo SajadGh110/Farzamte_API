d87bde3e62ee715b9ca680e4506468db7331898f
Evan <Evan.dev127@gmail.com>
2024-07-19 18:37:13 +0330

    -v3 HappyCall

--- FarzamTEWebsite/Controllers/HappyCallController.cs
+++ FarzamTEWebsite/Controllers/HappyCallController.cs
@@ -1,4 +1,5 @@
 ﻿using FarzamTEWebsite.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,15 +11,100 @@ namespace FarzamTEWebsite.Controllers
     {
         private IConfiguration _configuration;
         private FarzamDbContext _dbContext;
-
         public HappyCallController(FarzamDbContext dbContext, IConfiguration configuration)
         {
             _configuration = configuration;
             _dbContext = dbContext;
         }
 
+        [Authorize]
         [HttpGet]
-        public IActionResult Total_Count(DateTime startDate, DateTime endDate)
+        public IActionResult Customers_Count(DateTime startDate, DateTime endDate)
+        {
+            var CustomersCount = _dbContext.HappyCalls
+                .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate)
+                .GroupBy(hc => hc.CallTo)
+                .Count();
+            return Ok(CustomersCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult UniqueCustomers(DateTime startDate, DateTime endDate)
+        {
+            var UniqueCustomers = _dbContext.HappyCalls
+                .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate)
+                .Select(hc => hc.CallTo)
+                .Distinct()
+                .ToList();
+            if (UniqueCustomers == null)
+                return NotFound();
+            return Ok(UniqueCustomers);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Active_Customers_Count(DateTime startDate, DateTime endDate)
+        {
+            var ActiveCustomersCount = _dbContext.HappyCalls
+                .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate && hc.TradeStatus == "Active")
+                .Select(hc => hc.CallTo)
+                .Distinct()
+                .Count();
+            return Ok(ActiveCustomersCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Inactive_Customers_Count(DateTime startDate, DateTime endDate)
+        {
+            var InactiveCustomersCount = _dbContext.HappyCalls
+                .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate && hc.TradeStatus == "Inactive")
+                .Select(hc => hc.CallTo)
+                .Distinct()
+                .Count();
+            return Ok(InactiveCustomersCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult SuccessfulCalls_Count(DateTime startDate, DateTime endDate)
+        {
+            var SuccessfulCallsCount = _dbContext.HappyCalls
+                .Count(hc => hc.createdon >= startDate && hc.createdon <= endDate && hc.statusReason == "Made");
+            return Ok(SuccessfulCallsCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult ActiveAfterCalls_Count(DateTime startDate, DateTime endDate)
+        {
+            var ActiveAfterCallsCount = _dbContext.HappyCalls
+                .Count(hc => hc.createdon >= startDate && hc.createdon <= endDate && hc.TradeStatus == "Inactive" && hc.TradeStatusAffter == "Active");
+            return Ok(ActiveAfterCallsCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult ActiveInOtherBrockers_Count(DateTime startDate, DateTime endDate)
+        {
+            var ActiveInOtherBrockersCount = _dbContext.HappyCalls
+                .Count(hc => hc.createdon >= startDate && hc.createdon <= endDate && ( hc.CustomerRequirement == "فعال در کارگزاری دیگر" || hc.CustomerRequirement1 == "فعال در کارگزاری دیگر" || hc.CustomerRequirement2 == "فعال در کارگزاری دیگر"));
+            return Ok(ActiveInOtherBrockersCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult ExplanationClub_Count(DateTime startDate, DateTime endDate)
+        {
+            var ExplanationClubCount = _dbContext.HappyCalls
+                .Count(hc => hc.createdon >= startDate && hc.createdon <= endDate && hc.ExplanationClub == true);
+            return Ok(ExplanationClubCount);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Total_Count_Day(DateTime startDate, DateTime endDate)
         {
             var happyCallsByDay = _dbContext.HappyCalls
                 .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate)
@@ -31,8 +117,9 @@ namespace FarzamTEWebsite.Controllers
             return Ok(happyCallsByDay);
         }
 
+        [Authorize]
         [HttpGet]
-        public IActionResult SuccessfulCalls_Count(DateTime startDate, DateTime endDate)
+        public IActionResult SuccessfulCalls_Count_Day(DateTime startDate, DateTime endDate)
         {
             var SuccessfulCallsByDay = _dbContext.HappyCalls
                 .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate)
@@ -49,8 +136,9 @@ namespace FarzamTEWebsite.Controllers
             return Ok(SuccessfulCallsByDay);
         }
 
+        [Authorize]
         [HttpGet]
-        public IActionResult UnsuccessfulCalls_Count(DateTime startDate, DateTime endDate)
+        public IActionResult UnsuccessfulCalls_Count_Day(DateTime startDate, DateTime endDate)
         {
             var UnsuccessfulCallsByDay = _dbContext.HappyCalls
                 .Where(hc => hc.createdon >= startDate && hc.createdon <= endDate)
@@ -67,6 +155,7 @@ namespace FarzamTEWebsite.Controllers
             return Ok(UnsuccessfulCallsByDay);
         }
 
+        [Authorize]
         [HttpGet]
         public IActionResult All(DateTime startDate, DateTime endDate)
         {
